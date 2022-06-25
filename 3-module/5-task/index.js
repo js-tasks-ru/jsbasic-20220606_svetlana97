b2119b874bcd49f1962e2f60e23b98531ef17afb
Svetlana Lagutina <lagutinasvetlana@hotmail.com>
2022-06-25 20:13:47 +0300

    решения 3его модуля

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+ let result={ min:0, max:0 };
+ let arr = str.split(' ');
+ for (let i=0 ; i < arr.length; i++) {
+  if ((+arr[i] < result.min) && (typeof(+arr[i]) == 'number') && !isNaN(+arr[i])) {
+    result.min = +arr[i];
+  } else  if ((+arr[i] > result.max) && (typeof(+arr[i]) == 'number') && !isNaN(+arr[i])) {
+    result.max = +arr[i];
+  }
+ }
+ return result;
 }
